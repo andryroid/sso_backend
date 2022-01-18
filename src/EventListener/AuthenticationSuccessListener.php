1810f9b5a2a9b6b3a2898125bbf84fd164722fd5
RATSIMISETA Andry Lalaina <ratsimisetata@gmail.com>
2022-01-19 00:45:51 +0300

    add listener modifier token jwt

---
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\EventListener;
+
+
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+
+class AuthenticationSuccessListener
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * AuthenticationSuccessListener constructor.
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        $event->setData([
+            'id' => $event->getUser()->getId(),
+            'token' => $event->getData()['token']
+        ]);
+    }
+
+}
