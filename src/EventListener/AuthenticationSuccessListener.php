414a41da5228d67a104ed444270a3143fbabd728
RATSIMISETA Andry Lalaina <ratsimisetata@gmail.com>
2022-01-19 14:43:03 +0300

    add jwt created listener class

--- src/EventListener/AuthenticationSuccessListener.php
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -6,6 +6,8 @@ namespace App\EventListener;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\Security\Core\Security;
 
 class AuthenticationSuccessListener
 {
@@ -13,14 +15,20 @@ class AuthenticationSuccessListener
      * @var EntityManagerInterface
      */
     private $entityManager;
+    /**
+     * @var Security
+     */
+    private $security;
 
     /**
      * AuthenticationSuccessListener constructor.
      * @param EntityManagerInterface $entityManager
+     * @param Security $security
      */
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager,Security $security)
     {
         $this->entityManager = $entityManager;
+        $this->security = $security;
     }
 
     public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
@@ -29,6 +37,26 @@ class AuthenticationSuccessListener
             'id' => $event->getUser()->getId(),
             'token' => $event->getData()['token']
         ]);
+    }
+
+    /**
+     * @param JWTCreatedEvent $event
+     *
+     * @return void
+     */
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $request = $this->requestStack->getCurrentRequest();
+
+        $payload       = $event->getData();
+        $payload['id'] = $this->security->getUser()->getUserIdentifier();
+
+        $event->setData($payload);
+
+        /*$header        = $event->getHeader();
+        $header['cty'] = 'JWT';
+
+        $event->setHeader($header);*/
     }
 
 }
